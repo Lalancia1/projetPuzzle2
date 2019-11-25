cf91a121bf7ce75fec988c776dc1d7fd6eae819e
Bonnet Michèle <michelebonnet59550@gmail.com>
2019-11-25 09:31:28 +0100

    mmmmmmmmmmmmmmmmm

--- projetPuzzle.js
+++ projetPuzzle.js
@@ -17,109 +17,57 @@ var verbes = ['Danser' ,'Frapper' ,'Lubrifier', 'Trikiter', 'Procrastiner', 'Fou
     'Recoudre', 'Punir', 'Attaquer', 'Coder', 'Jouer', 'Manger', 'Avoir'];//15
 //**********************************************************************************************************************************
 
-//pour les variables aleatoire des noms
-var NameIntervalNumber=Math.random();
-var NameNombres= (Math.floor(NameIntervalNumber*name.length));
-console.log(NameNombres+ "math 1");
-//***************************************************************************************************************************
 //pour les objets
-function longueurTab(tableau) {
+function parcourirTab(tableau) {
     var numero = Math.random();
-
-    var numeroSansVirgule = (Math.floor(numero*tableau.length));
-    console.log(objetsNombres+ "math 2");
-    console.log(objetsNombres);
-    return  objetsNombres;
+    var objetsNombres = (Math.floor(numero*tableau.length));
+    return objetsNombres;
 
 }
- var numeroLieu= longueurTab(lieux);
+
+//******************************************************************************************************
+var ajouterUl = document.getElementById('ajouter');
+var  phrases="x";
+
+var add = -1;
+
+//*******************************************************************************************
+ var numeroLieu= parcourirTab(lieux);
 console.log("ici lieux "+numeroLieu);
-var NameNombres= longueurTab(noms);
-
-
-//*********************************************************************************************
-var nameIntervalNumber=Math.random();
-var nameNombres= (Math.floor(nameIntervalNumber*noms.length));
-console.log("MON NOM"+nameNombres);
-//************************************************************************************************
-var objetsIntervalNumber=Math.random();
-var objetsNombres= (Math.floor(objetsIntervalNumber*objet.length));
+var NameNombres= parcourirTab(noms);
 
 //*****************************************************************************************************************************
-//pour les temperatures
-var temperaturesIntervalNumber=Math.random();
-var temperaturesNombres= (Math.floor(temperaturesIntervalNumber*temperatures.length));
-//******************************************************************************************************************************
-//pour les lieux
-var placeIntervalNumber=Math.random();
-var placeNombres= (Math.floor( placeIntervalNumber*lieux.length));
-//****************************************************************************************************************************
-//vpour les verbes
-var verbeIntervalNumber=Math.random();
-var verbeNombres= (Math.floor( verbeIntervalNumber*verbes.length));
-//****************************************************************************************************************************
-/*association des elements et leurs index(fatiguée j'ai créé à nouveau des tableaux alors que l'exercice nous en fournissait :donc les noms
-des tableaux sont:noms;objet;temperatures;lieux;verbes*************array[i]ici array=nom,i=nameNombre
- */
 
-var associateNameIndex= noms[NameNombres];
-alert(associateNameIndex);
-
-
-//********************************************************************************************************************
-//création du container pour l'evenement
-var body=document.body;
-var createDivContainer=document.createElement("div");
-createDivContainer.setAttribute('id','container');
-createDivContainer.setAttribute('class','reste');
-createDivContainer.setAttribute('style','border:5px orange solid;width:800px;height:700px;');
-body.appendChild(createDivContainer);
-//*****************************************************************************************************************
-
-var idInput=document.getElementById('entrez');
-var idBouton= document.getElementById('HISTOIRE');
-
-function Generer_une_histoire(){
+//function Generer_une_histoire(){
+idBouton=document.getElementById("HISTOIRE");
 idBouton.addEventListener("click",function(){
-   // alert("button");
-    for(var i=0;i<=name.length;i++){
-        for( j=0; j<=objet.length; j++){
-            for(var k=0;k<=temperatures.length;k++){
-                for(var l=0; l<=lieux.length; l++){
-                    console.log(lieux[numeroLieu]);
-                  for( var m=0;m<=verbes.length;m++){
-var idContainer=document.getElementById('container');
-var idAjouter=document.getElementById('ajouter');
-if(idInput.value) {
+    var idInput=document.getElementById('entrez');//user
+       var userName=idInput.value;
 
-    idContainer.innerHTML += 'il était une fois,' + " " + noms[nameNombres] + " " + "et" + " " + idInput.value + " " + "avaient été" + " " + verbes[verbeNombres] + " " + "par" + " " + temperatures[temperaturesNombres] + " " + "de température" + ";" + " " + 'sa maison se trouvait dans' + " " + lieux[placeNombres] + " " + "et" + " " + 'contenait' + " " + "des" + " " + objet[objetsNombres] + '.';
+    var temperaturesNombres=parcourirTab(temperatures);
+    var  verbeNombres=parcourirTab(verbes);
+    var  placeNombres = parcourirTab(lieux);
+    var nameNombres= parcourirTab(noms);
+    var objetsNombres= parcourirTab(objet);
 
-}
 
-                  }
-                }
-            }
-        }}
 
+                        if (userName) {
+
+                            phrases = 'il était une fois,' + " " + noms[nameNombres] + " " + "et" + " " + idInput.value + " " + "avaient été" + " " + verbes[verbeNombres] + " " + "par" + " " + temperatures[temperaturesNombres] + " " + "de température" + ";" + " " + 'sa maison se trouvait dans' + " " + lieux[placeNombres] + " " + "et" + " " + 'contenait' + " " + "des" + " " + objet[objetsNombres] + '.';
+
+                            var li = document.createElement('li');
+                            li.innerHTML = phrases;
+                            ajouterUl.appendChild(li);
+
+
+                        }
 });
-}
-Generer_une_histoire();
-function ajout(){
 
 
-        if(idInput.value) {
-        var textGarder=document.createTextNode('il était une fois,' + " " + noms[nameNombres] + " " + "et" + " " + idInput.value + " " + "avaient été" + " " + verbes[verbeNombres] + " " + "par" + " " + temperatures[temperaturesNombres] + " " + "de température" + ";" + " " + 'sa maison se trouvait dans' + " " + lieux[placeNombres] + " " + "et" + " " + 'contenait' + " " + "des" + " " + objet[objetsNombres] + '.');
-        idContainer.appendChild(textGarder);
 
-    }if(idInput.value===""){
-        idInput.value;
-        Generer_une_histoire();
-    }
-        i++;
-}
-ajout();
-   /*
-   window.onload = function() { init1(); init2(); };pour le rechargement
+/*
+
 
     * for(var i = 0; i<objet.length; i++){
     *
@@ -132,11 +80,6 @@ ajout();
         * }
 
     * }
-    *
-    *
-    *
-    *
-    *
-    * */
+     */
 
 
